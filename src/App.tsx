c00cfb2fd837f01e4bb847f1259b94927adb5a23
Hancharou <pl.hancharou@gmail.com>
2025-01-30 14:33:44 +0100

    feat: implement CustomButton component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { Component } from 'react';
 import './App.css';
+import CustomButton from './components/CustomButton/CustomButton.tsx';
 
-function App() {
-  const [count, setCount] = useState(0);
+interface AppState {
+  triggerError: boolean;
+}
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+class App extends Component<object, AppState> {
+  state: AppState = { triggerError: false };
+
+  throwAnError = () => {
+    this.setState((state) => ({ ...state, triggerError: true }));
+  };
+
+  render() {
+    if (this.state.triggerError) throw new Error('Error during button click!');
+
+    return (
+      <>
+        <CustomButton onClick={this.throwAnError}>Throw An Error</CustomButton>
+      </>
+    );
+  }
 }
 
 export default App;
