4bb174d90960e5ea4f3e205cb65012dd2ad7f55d
Hancharou <pl.hancharou@gmail.com>
2025-02-05 09:07:04 +0100

    refactor: improve models types

--- src/models/PostsResponse.d.ts
+++ src/models/PostsResponse.d.ts
@@ -1,8 +1,16 @@
 import { Post } from './Post';
+import { FullPost } from './FullPost';
 
-export interface PostsResponse {
-  posts: Post[];
+interface BaseResponse {
   total: number;
   skip: number;
   limit: number;
 }
+
+export type TypePosts = { posts: Post[] };
+export type TypeComments = Pick<FullPost, 'comments'>;
+
+type ApiResponse<T> = BaseResponse & T;
+type ResponseTypes = TypePosts | TypeComments;
+
+export type FetchResponse<T> = T extends ResponseTypes ? ApiResponse<T> : T;
--- src/models/Query.d.ts
+++ src/models/Query.d.ts
@@ -1,8 +1,12 @@
 import { Post } from './Post';
-import { PostsResponse } from './PostsResponse';
+import { User } from './User';
+import { BaseResponse } from './PostsResponse';
 
-export interface PostsQueryParams
-  extends Pick<PostsResponse, 'limit' | 'skip'> {
-  select: Array<keyof Post>;
+export interface PostsQueryParams extends Pick<BaseResponse, 'limit' | 'skip'> {
+  select: Required<Array<keyof Post>>;
   q?: string;
+}
+
+export interface UserQueryParams {
+  select: Array<keyof User>;
 }
