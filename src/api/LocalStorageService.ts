dff58640bde7f7522cbd86227c6ed7c63f516497
Hancharou <pl.hancharou@gmail.com>
2025-02-08 14:48:42 +0100

    perf: add getter/setter detailsParams to LS service

--- src/api/LocalStorageService.ts
+++ src/api/LocalStorageService.ts
@@ -1,12 +1,27 @@
-class LocalStorageService {
-  #searchTerm = 'searchTerm';
+import { PostsQueryParams } from '../models';
 
-  get searchTerm() {
-    return localStorage.getItem(this.#searchTerm) || '';
+type SearchParamsType = Omit<PostsQueryParams, 'select'>;
+
+class LocalStorageService {
+  #searchParams = 'searchParams';
+  #detailsParams = 'detailsParams';
+
+  get searchParams() {
+    const data = localStorage.getItem(this.#searchParams);
+    if (!data) return { skip: 0, limit: 25 };
+    return JSON.parse(data);
   }
 
-  set searchTerm(term: string) {
-    localStorage.setItem(this.#searchTerm, term);
+  set searchParams(term: SearchParamsType) {
+    localStorage.setItem(this.#searchParams, JSON.stringify(term));
+  }
+
+  get detailsParams() {
+    return localStorage.getItem(this.#detailsParams) || '';
+  }
+
+  set detailsParams(term: string) {
+    localStorage.setItem(this.#detailsParams, term);
   }
 }
 
